ef4ca819509c4803191e1fe6f0f70553378b3cef
hnrqth <henriquesthones@gmail.com>
2025-03-14 01:17:04 +0000

    codigo do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Função para entrada dos dados
-void entrada(char estado[20], char cod[20], char cidade[20], int *populacao, float *area, float *pib, int *pontosTuristicos) {
+void entrada(char estado[20], char cod[10], char cidade[20], int *populacao, float *area, float *pib, int *pontosTuristicos) {
     printf("Digite o Estado: ");
     scanf("%s", estado);  
 
@@ -25,20 +25,20 @@ void entrada(char estado[20], char cod[20], char cidade[20], int *populacao, flo
 }
 
 // Função para exibir os dados
-void saida(char estado[20], char cod[20], char cidade[20], int populacao, float area, float pib, int pontosTuristicos) {
+void saida(char estado[20], char cod[10], char cidade[20], int populacao, float area, float pib, int pontosTuristicos) {
     printf("\nEstado: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nNúmero de pontos turísticos: %d\n",
            estado, cod, cidade, populacao, area, pib, pontosTuristicos);
 }
 
 int main() {
     // Carta 1
-    char estado1[20], cod1[20], cidade1[20];
+    char estado1[20], cod1[10], cidade1[20];
     int populacao1;
     float area1, pib1;
     int pontosTuristicos1;
 
     // Carta 2
-    char estado2[20], cod2[20], cidade2[20];
+    char estado2[20], cod2[10], cidade2[20];
     int populacao2;
     float area2, pib2;
     int pontosTuristicos2;
